4a281b78ee7ea4084a9e461721becc087b1a769c
Jose <jcantillo@ggc.edu>
2021-11-23 12:27:16 -0500

    Initial Commit

---
+++ RCSInventory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RCSInventory
+//
+//  Created by Jose Cantillo on 11/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
